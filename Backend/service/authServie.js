3c4d02dc5b3d84c5f1a5f82cfdcd8b84fc8f316d
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-09-22 15:20:40 +0530

    added auth service file

---
+++ Backend/service/authServie.js
@@ -0,0 +1,51 @@
+// services/authService.js
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { SECRET_KEY, HR_EMAIL, HR_PASSWORD } = process.env;
+
+const registerUser = async (username, password) => {
+  const existingUser = await User.findOne({ username });
+  if (existingUser) {
+    throw new Error("Username already exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({
+    username,
+    password: hashedPassword,
+    role: "employee",
+  });
+  await newUser.save();
+
+  return { message: "User registered successfully" };
+};
+
+const loginUser = async (username, password) => {
+  // Check HR login
+  if (username === HR_EMAIL && password === HR_PASSWORD) {
+    const token = jwt.sign({ id: "HR", role: "hr" }, SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    return { token };
+  }
+
+  // Employee login
+  const user = await User.findOne({ username });
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new Error("Invalid credentials");
+  }
+
+  const token = jwt.sign({ id: user._id, role: "employee" }, SECRET_KEY, {
+    expiresIn: "1d",
+  });
+
+  return { token };
+};
+
+module.exports = { registerUser, loginUser };
